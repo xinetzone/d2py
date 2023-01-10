8c35359926836e66eadc5d552c680be4341a8e37
xinetzone <xinzone@outlook.com>
2023-01-10 20:20:13 +0800

    更新

--- tasks.py
+++ tasks.py
@@ -1,3 +1,8 @@
+import inspect
+
+if not hasattr(inspect, 'getargspec'): # 修复
+    inspect.getargspec = inspect.getfullargspec
+    
 from d2py.tools.write import site
 
 namespace = site('doc', target='doc/_build/html')
