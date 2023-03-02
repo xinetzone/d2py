e2af937afe142cdaa43febd5489f4b4272db126e
xinetzone <xinzone@outlook.com>
2023-03-02 15:57:45 +0800

    更新

--- doc/conf.py
+++ doc/conf.py
@@ -165,12 +165,10 @@ gettext_compact = False  # optional.
 # 定制主侧栏
 html_sidebars = {
     "*": [
-        "sidebar-logo.html",
         "search-field.html",
         "sbt-sidebar-nav.html",
     ],
     "posts/**": [
-        "sidebar-logo.html",
         "postcard.html",
         "recentposts.html",
         "tagcloud.html",
@@ -232,6 +230,58 @@ html_theme_options = {
         "notebook_interface": "jupyterlab",
         "thebe": True,  # Thebe 实时代码单元格
     },
+    # 图标可以参考 https://fontawesome.com/icons
+    "icon_links": [
+        # {
+        #     "name": "GitHub",
+        #     "url": "https://github.com/xinetzone/d2py",
+        #     "icon": "fa-brands fa-square-github",
+        #     "type": "fontawesome",
+        # },
+        {
+            "name": "知乎",
+            "url": "https://www.zhihu.com/people/xinetzone",
+            "icon": "fa-brands fa-zhihu",
+            "type": "fontawesome",
+        },
+        {
+            "name": "简书",
+            "url": "https://www.jianshu.com/u/4302480a3e8e",
+            "icon": "https://xinetzone.github.io/xinetzone/media/jianshu.svg",
+            "url": "url",
+        },
+        {
+            "name": "博客园",
+            "url": "https://www.cnblogs.com/q735613050/",
+            "icon": "https://xinetzone.github.io/xinetzone/media/xinetzone.jpg",
+            "type": "url",
+        },
+        {
+            "name": "领英",
+            "url": "https://www.linkedin.com/in/xinet",
+            "icon": "fa-brands fa-linkedin",
+            "type": "fontawesome",
+        }
+        # {
+        #     "name": "GitLab",
+        #     "url": "https://gitlab.com/<your-org>/<your-repo>",
+        #     "icon": "fa-brands fa-square-gitlab",
+        #     "type": "fontawesome",
+        # },
+        # {
+        #     "name": "Twitter",
+        #     "url": "https://twitter.com/<your-handle>",
+        #     "icon": "fa-brands fa-square-twitter",
+        #     # The default for `type` is `fontawesome` so it is not actually required in any of the above examples as it is shown here
+        # },
+        # {
+        #     "name": "Mastodon",
+        #     "url": "https://<your-host>@<your-handle>",
+        #     "icon": "fa-brands fa-mastodon",
+        # },
+    ],
+    # "footer_start": ["copyright", "sphinx-version"],
+    "footer_end": ["last-updated", ],
 }
 # -- 自定义网站的标志 --------------
 html_logo = 'logo.jpg'
