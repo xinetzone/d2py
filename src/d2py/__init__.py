7921d25749f528975570c3bc40db5d24ff62c760
xinetzone <xinzone@outlook.com>
2023-03-14 19:18:52 +0800

    修改：     src/d2py/quantum/graph/tree.py

--- src/d2py/__init__.py
+++ src/d2py/__init__.py
@@ -1,2 +1,2 @@
 """Dive into Python."""
-__version__ = '0.3.6'
+__version__ = '0.3.7'
--- src/d2py/quantum/graph/tree.py
+++ src/d2py/quantum/graph/tree.py
@@ -4,6 +4,6 @@
 class Bunch(dict):
     """将 dict 对象切片模式转换为点抽取。
     """
-    def __init__(self, bunch: dict, /, **kwargs):
-        super().__init__(bunch, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.__dict__ = self
