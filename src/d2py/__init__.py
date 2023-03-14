69a548dab33e0a6397b7f9ccd7b0c133bcf15e1b
xinetzone <xinzone@outlook.com>
2023-03-14 19:14:26 +0800

    修复 Bunch

--- src/d2py/__init__.py
+++ src/d2py/__init__.py
@@ -1,2 +1,2 @@
 """Dive into Python."""
-__version__ = '0.3.5'
+__version__ = '0.3.6'
--- src/d2py/quantum/graph/tree.py
+++ src/d2py/quantum/graph/tree.py
@@ -1,9 +1,9 @@
-from collections import UserDict
+"""树结构"""
 
 
-class Bunch(UserDict):
+class Bunch(dict):
     """将 dict 对象切片模式转换为点抽取。
     """
-    def __init__(self, dict=None, /, **kwargs):
-        super().__init__(dict, **kwargs)
+    def __init__(self, bunch: dict, /, **kwargs):
+        super().__init__(bunch, **kwargs)
         self.__dict__ = self
