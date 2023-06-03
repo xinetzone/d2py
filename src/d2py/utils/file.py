216d9b2bbb35300b2e8c3b93c21a4a6d50ce0712
xinetzone <xinzone@outlook.com>
2023-06-03 18:15:54 +0800

    修改：     doc/pythonx/basic/logging.ipynb

--- src/d2py/utils/file.py
+++ src/d2py/utils/file.py
@@ -1,9 +1,15 @@
 '''File Processing Tools
 '''
 from pathlib import Path
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def mkdir(root):
     root = Path(root)
     if not root.exists():
         root.mkdir(mode=511, parents=True, exist_ok=True)
+        logger.info(f"{root} 创建成功")
+    else:
+        logger.info(f"{root} 已存在无需创建")
