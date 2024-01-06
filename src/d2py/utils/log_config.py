7efb31e2fbc92a9e757d3f0838751ca495c58bcf
liuxinwei <liuxinwei@xmsilicon.cn>
2024-01-06 11:59:25 +0800

    完善   src/d2py/utils/log_config.py

--- src/d2py/utils/log_config.py
+++ src/d2py/utils/log_config.py
@@ -2,21 +2,26 @@ import logging
 
 
 def config_logging(
-    filename,
-    logger_name="logger",
-    filemode='a',
-    filter_mod_names = [
+    filename: str,
+    logger_name: str="logger",
+    filemode: str='a',
+    filter_mod_names: set=set(),
+    default_filter_mod_names: set = {
         "matplotlib", "PIL", "asyncio", 
         "urllib3", "markdown_it",
         "ablog"
-    ],
-    file_formatter="%(levelname)s|%(asctime)s|%(name)s| -> %(message)s\n|==>%(module)s.%(funcName)s@: %(pathname)s:%(lineno)d",
-    stream_formatter="%(levelname)s|%(asctime)s|%(name)s| -> %(message)s",
+    },
+    file_formatter: str="%(levelname)s|%(asctime)s|%(name)s| -> %(message)s\n|==>%(module)s.%(funcName)s@: %(pathname)s:%(lineno)d",
+    stream_formatter: str="%(levelname)s|%(asctime)s|%(name)s| -> %(message)s",
     ):
     """配置 logging
 
     Args:
-        path: 日志保存路径
+        filename: 日志保存路径
+        logger_name: 日志名称
+        filemode: 写入到文件的日志写入模式
+        filter_mod_names: 自定义过滤日志 debug 模式对应的模块名称列表
+        default_filter_mod_names: 默认过滤日志 debug 模式对应的模块名称列表
         file_formatter: 日志文件配置
         stream_formatter: 控制台打印配置
     """
@@ -27,7 +32,7 @@ def config_logging(
                         filemode=filemode)
     
     # 禁用一些 debug 信息
-    for mod_name in filter_mod_names:
+    for mod_name in filter_mod_names|default_filter_mod_names:
         _logger = logging.getLogger(mod_name)
         _logger.setLevel(logging.WARNING)
     
