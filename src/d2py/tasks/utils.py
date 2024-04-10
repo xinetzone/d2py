e178755ffd7bb002123ddffe34d03763a735eb2c
liuxinwei <liuxinwei@xmsilicon.cn>
2024-04-10 15:58:11 +0800

    修改：     src/d2py/tasks/utils.py

--- src/d2py/tasks/utils.py
+++ src/d2py/tasks/utils.py
@@ -1,12 +1,17 @@
 """任务型事务辅助函数"""
 import asyncio
+import textwrap
 
-async def run_cmd(cmd: str, cwd: str=".", **kwds):
+async def run_cmd(cmd: str, cwd: str|None=None, indent: int=2,  **kwds)->str:
     """运行命令行执行任务
 
     Args:
         cmd: 命令行任务
         cwd: 当前工作目录
+        indent: 命令行打印信息缩进位数
+
+    Return:
+        返回命令任务执行的打印信息
     """
     proc = await asyncio.create_subprocess_shell(
         cmd, 
@@ -17,7 +22,7 @@ async def run_cmd(cmd: str, cwd: str=".", **kwds):
     stdout, stderr = await proc.communicate(input=None)
     std_data = f'[{cmd!r} 退出状态码 {proc.returncode}]\n'
     if stdout:
-        std_data += f'[stdout]:\n{stdout}\n'
+        std_data += f'[stdout]:\n{textwrap.indent(stdout.decode(), ' '*indent)}\n' 
     if stderr:
-        std_data += f'[stdinfo]:\n{stderr}\n'
+        std_data += f'[stdinfo]:\n{textwrap.indent(stderr.decode(), ' '*indent)}\n'
     return std_data
