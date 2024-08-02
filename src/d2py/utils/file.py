f6065d616ccf76c11e653cdcc21199fdadfb59cc
liuxinwei <liuxinwei@xmsilicon.cn>
2024-08-02 09:53:21 +0800

    修改：     src/d2py/utils/file.py

--- src/d2py/utils/file.py
+++ src/d2py/utils/file.py
@@ -4,11 +4,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def mkdir(root: str|Path):
+def mkdir(root: str|Path, mode=777, parents=True, exist_ok=True):
     """创建文件夹"""
     root = Path(root)
     if not root.exists():
-        root.mkdir(mode=511, parents=True, exist_ok=True)
+        root.mkdir(mode=mode, parents=parents, exist_ok=exist_ok)
         logger.info(f"{root} 创建成功")
     else:
         logger.info(f"{root} 已存在无需创建")
