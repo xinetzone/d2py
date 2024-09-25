b19e1b7719df0a7272d623b39d442b393d71f0ad
liuxinwei <liuxinwei@xmsilicon.cn>
2024-09-25 08:39:07 +0800

    更新文档

--- doc/library/ctypes/test.c
+++ doc/library/ctypes/test.c
@@ -11,27 +11,11 @@ int gcd(int x, int y) {
     return g;
 }
 
-
-// 用于检测一个点是否在Mandelbrot集合中的函数
-int in_mandel(double x0, double y0, int n) {
-    double x=0,y=0,xtemp;
-    while (n > 0) {
-        xtemp = x*x - y*y + x0;
-        y = 2*x*y + y0;
-        x = xtemp;
-        n -= 1;
-        if (x*x + y*y > 4) return 0;
-    }
-    return 1;
-}
-
-
 int divide(int a, int b, int *remainder) {
     int quot = a / b;
     *remainder = a % b;
     return quot;
 }
-
 
 double avg(double *a, int n) {
     int i;
@@ -42,11 +26,9 @@ double avg(double *a, int n) {
     return total / n;
 }
 
-
 typedef struct Point {
     double x, y;
 } Point;
-
 
 double distance(Point *p1, Point *p2) {
     return hypot(p1->x - p2->x, p1->y - p2->y);
